279e9294d016954987c341ba84d536a1d49a5659
purplebyte <alfarodarwayne@gmail.com>
2018-10-10 22:49:43 +0800

    correcting input placeholder again

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -13,7 +13,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 					<input 
 					className='f4 pa2 w-70 center' 
 					type='text'
-					placeholder='Enter URL' 
+					placeholder='Enter Image URL' 
 					onChange={onInputChange}
 				/>
 					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
